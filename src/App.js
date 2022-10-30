9a89ff6ceb990d2264e68d0720c881b28e37b2a1
hilaryyates <hilryyates@gmail.com>
2022-10-30 14:07:37 -0400

    func sign out is button

--- src/App.js
+++ src/App.js
@@ -21,7 +21,6 @@ class App extends Component {
         {this.state.route === "signed-out" ? (
           <div>
             <SignIn onRouteChange={this.onRouteChange} />
-            <Register />
           </div>
         ) : (
           <div>
@@ -37,40 +36,4 @@ class App extends Component {
 
 export default App;
 
-// import { Component } from "react";
-// import "./index.css";
-// import "./components/SearchForm";
-// import SignIn from "./components/SignIn";
-// import Register from "./components/Register";
-// import SearchForm from "./components/SearchForm";
-// import Logo from "./Logo";
-// import SignOut from "./components/SignOut";
-
-// class App extends Component {
-//   constructor() {
-//     super();
-//     this.state = { route: "signed-out" };
-//   }
-//   onRouteChange = () => {
-//     this.setState({ route: "home" });
-//   };
-//   render() {
-//     return (
-//       <div class='main'>
-//         {this.state.route === "signed-out" ? (
-//           <div>
-//             <SignIn onRouteChange={this.onRouteChange} />
-//           </div>
-//         ) : (
-//           <div>
-//             <SignOut />
-//             <Logo />
-//             <SearchForm />
-//           </div>
-//         )}
-//       </div>
-//     );
-//   }
-// }
-
-// export default App;
+// register button triggers register form
--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -3,7 +3,7 @@ import React from "react";
 const Navigation = ({ onRouteChange }) => {
   return (
     <div>
-      <p onClick={() => onRouteChange("signed-out")}>Sign Out</p>
+      <button onClick={() => onRouteChange("signed-out")}>Sign Out</button>
     </div>
   );
 };
