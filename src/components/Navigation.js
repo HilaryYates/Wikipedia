3d3b6645022832cb5d9c91acdc6c73929ab09b78
hilaryyates <hilryyates@gmail.com>
2022-11-10 22:17:15 -0500

    notes

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -4,10 +4,11 @@ import Button from "react-bootstrap/Button";
 const Navigation = ({ onRouteChange }) => {
   return (
     <div>
+      // route is not logged-in // option to route to register // option to
+      route to login // route is logged-in? option to sign out
       <Button variant='primary' onClick={() => onRouteChange("signed-out")}>
         Sign Out
       </Button>
-
       {/* <button onClick={() => onRouteChange("signed-out")}>Sign Out</button> */}
     </div>
   );
