d2c6f55584ced2ef4cecbd63d69a1ba0d04cc1bb
hilaryyates <hilryyates@gmail.com>
2022-11-11 10:14:45 -0500

    buttons on home page

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,15 +1,36 @@
 import React from "react";
 import Button from "react-bootstrap/Button";
+import Register from "./Register";
+import SignIn from "./Register";
 
 const Navigation = ({ onRouteChange }) => {
   return (
     <div>
-      // route is not logged-in // option to route to register // option to
-      route to login // route is logged-in? option to sign out
+      {/* <Router>
+        <Routes>
+          <Route path='./SignIn.js' element={SignIn} />
+          <Route path='./Register.js' element={Register} />
+        </Routes>
+      </Router>
+      <div> */}
+      <Button
+        variant='primary'
+        to='./SignIn.js'
+        onClick={() => onRouteChange("signed-out")}
+      >
+        Sign in
+      </Button>
+      <Button
+        variant='primary'
+        to='./Register.js'
+        onClick={() => onRouteChange("signed-out")}
+      >
+        {" "}
+        Sign In
+      </Button>
       <Button variant='primary' onClick={() => onRouteChange("signed-out")}>
         Sign Out
       </Button>
-      {/* <button onClick={() => onRouteChange("signed-out")}>Sign Out</button> */}
     </div>
   );
 };
