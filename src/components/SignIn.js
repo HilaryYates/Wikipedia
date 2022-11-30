b67e711899c077c2a60dc6112e441633a9e67e87
hilaryyates <hilryyates@gmail.com>
2022-11-29 19:17:02 -0500

    signin register components working

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -23,8 +23,8 @@ class SignIn extends React.Component {
     })
       .then((response) => response.json())
       .then((user) => {
-        console.log(user.id, this.props.loadUser);
-        if (user.id) {
+        console.log(user);
+        if (user) {
           this.props.loadUser(user);
           this.props.onRouteChange("home");
         }
